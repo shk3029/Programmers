6c89a95f45835f8c8677c816513a4f1f55116993
JS <shk3029>
2019-10-19 16:41:42 +0900

    add TEST description

--- test/com/company/lv2/hash/CamouflageTest.java
+++ test/com/company/lv2/hash/CamouflageTest.java
@@ -1,11 +1,26 @@
 package com.company.lv2.hash;
 
+import com.company.common.TestDescription;
 import org.junit.Test;
 
 public class CamouflageTest {
+
     @Test
-    public void camouflageTest() {
+    @TestDescription("통합 테스트")
+    public void all_Test() {
         System.out.println(">>>> Hash -위장 테스트 추가 ");
+    }
+
+    @Test
+    @TestDescription("기능 테스트 1 : ")
+    public void functional_Test_1() {
 
     }
+
+    @Test
+    @TestDescription("기능 테스트 2 : ")
+    public void functional_Test_2() {
+
+    }
+
 }
