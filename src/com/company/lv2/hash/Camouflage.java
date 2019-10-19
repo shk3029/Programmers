49368be79728d4b06f97d82a340ddf924a92b2f4
JS <shk3029>
2019-10-19 16:35:36 +0900

    Add Junit

---
+++ src/com/company/lv2/hash/Camouflage.java
@@ -0,0 +1,8 @@
+package com.company.lv2.hash;
+
+// 위장
+public class Camouflage {
+    public void Solution() {
+
+    }
+}
---
+++ test/com/company/lv2/hash/CamouflageTest.java
@@ -0,0 +1,11 @@
+package com.company.lv2.hash;
+
+import org.junit.Test;
+
+public class CamouflageTest {
+    @Test
+    public void camouflageTest() {
+        System.out.println(">>>> Hash -위장 테스트 추가 ");
+
+    }
+}
