94b86d1cf46972cbacba87654f5bf091ac3cc743
shk3029 <shk30290@naver.com>
2020-10-23 00:39:55 +0900

    SubarrayDivision

--- src/com/company/hackerrank/SubarrayDivision.java
+++ src/com/company/hackerrank/SubarrayDivision.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class SubarrayDivision {
     public static void main(String[] args) {
-        birthday(List.of(1,1, 1, 1, 1, 1), 3, 2);
+        birthday2(List.of(1,2,1,3,2), 3, 2);
     }
     // Complete the birthday function below.
     static int birthday(List<Integer> s, int d, int m) {
@@ -15,6 +15,17 @@ public class SubarrayDivision {
                 if (i+m <= s.size()) sum += s.get(j);
             }
             if (d==sum) result++;
+        }
+        return result;
+    }
+
+    static int birthday2(List<Integer> s, int d, int m) {
+        int result = 0;
+        int sum = 0;
+        for (int i = 0; i < s.size(); i++) {
+            sum += s.get(i);
+            if (i>=m) sum -= s.get(i - m);
+            if (i>=m-1 && sum == d) result++;
         }
         return result;
     }
