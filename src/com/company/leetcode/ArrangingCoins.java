5d793378d6694c7dc4389108cec9e3f9598a4ccf
shk3029 <shk30290@naver.com>
2020-10-28 00:22:32 +0900

    바이너리서치

--- src/com/company/leetcode/ArrangingCoins.java
+++ src/com/company/leetcode/ArrangingCoins.java
@@ -17,20 +17,20 @@ public class ArrangingCoins {
         return i;
     }
 
-    public static int arrangeCoins_binarysearch(int n) {
-        int left = 1;
-        int right = n;
+public static int arrangeCoins_binarysearch(int n) {
+    int left = 1;
+    int right = n;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if ((mid * (mid + 1L)) / 2L <= n) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if ((mid * (mid + 1L)) / 2L <= n) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
-        return right;
     }
+    return right;
+}
 
 
 
