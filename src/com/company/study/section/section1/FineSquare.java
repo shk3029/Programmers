e3ec0cdeb897e398922eaa77638b31224a9ba2a1
shk3029 <shk30290@naver.com>
2020-11-10 23:39:38 +0900

    프로그래머스 멀쩡한사각형완료!

--- src/com/company/study/section/section1/FineSquare.java
+++ src/com/company/study/section/section1/FineSquare.java
@@ -22,8 +22,18 @@ package com.company.study.section.section1;
 
 
 public class FineSquare {
-    public long solution(int w, int h) {
-        long answer = 1;
-        return answer;
+
+    public static void main(String[] args) {
+        System.out.println(solution(8,12));
     }
+    public static long solution(int w, int h) {
+        long answer = 0;
+        for (int x=0; x<w; x++) {
+            long y = ((long) h * x) / (long) w;
+            answer+=y;
+        }
+        return answer*2;
+    }
+
+
 }
