9b8734ad2a07290997f56932462b9bfef27d707f
shk3029 <shk30290@naver.com>
2020-11-15 12:22:32 +0900

    MaximumSubarray

--- src/com/company/study/section/section2/MaximumSubarray.java
+++ src/com/company/study/section/section2/MaximumSubarray.java
@@ -12,6 +12,9 @@ import java.util.Arrays;
  * dp 문제
  *
  */
+
+// nums : -2 1 -3 4 -1 2 1 -5 4
+// dp   : -2 1 -2 4  3 5 6  1 5
 public class MaximumSubarray {
     public static void main(String[] args) {
         int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
@@ -19,25 +22,32 @@ public class MaximumSubarray {
     }
 
     public static int maxSubArray(int[] nums) {
-        System.out.println("start");
-        System.out.println(Arrays.toString(nums));
-
         int[] dp = new int[nums.length];
-
-        int sum = Arrays.stream(nums).sum();
-        System.out.println(sum);
-        int max = 6;
-        // nums : -2 1 -3 4 -1 2 1 -5 4
-        // dp   : -2 1 -2 4  3 5 6  1 5
-
-        // 이전 숫자 - 이면,
-        //
-        // 이전 숫자 + 이면,
-        //
-
-        for (int num : nums) {
-
+        int max = nums[0];
+        dp[0] = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            if (dp[i-1] < 0) {
+                dp[i] = nums[i];
+            } else {
+                dp[i] = nums[i] + dp[i-1];
+            }
+            max = dp[i] > max ? dp[i] : max;
         }
-        return 0;
+
+        return max;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
